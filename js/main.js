92ec4bc7f4a4b619a8dcc3ad52ab140659bca369
Alex Perkins <acperkins@gmail.com>
2016-04-19 16:48:12 -0400

    epic mode working

--- js/main.js
+++ js/main.js
@@ -1,42 +1,77 @@
 (function() {
     'use strict';
 
-
-    var animals = [
-      {species: 'Buffalo', type: 'Mammal', picture: 'https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg'},
-      {species: 'Penguin', type: 'Bird', picture: 'http://searchengineland.com/figz/wp-content/seloads/2014/08/penguin-walking-ss-1920-800x450.jpg'},
-      {species: 'Platypus', type: '???', picture: 'http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/platypus_662_600x450.jpg'}
-    ];
-
     angular
-      .module('zoo-thing', [])
-      .controller('WelcomeController', WelcomeController)
-      .controller('AnimalsController', AnimalsController);
+      .module('shopulate', [])
+      .controller('InventoryController', InventoryController);
 
-      function WelcomeController() {
-        this.name = 'World';
-        this.now = new Date();
-      }
 
-      function AnimalsController() {
+      function InventoryController() {
           this.all = [
-             { species: "Buffalo", type: "Mammal", picture: "https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg" },
-             { species: "Penguin", type: "Bird", picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg/800px-South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg" },
-             { species: "Platypus", type: "???", picture: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Wild_Platypus_4.jpg" },
-             { species: "Ostrich", type: "Bird", picture: "" },
-             { species: "Bear", type: "Mammal", picture: "" },
-             { species: "Baboon", type: "Mammal", picture: "" },
-             { species: "T-Rex", type: "Dinosaur", picture: "" },
-             { species: "Velociraptor", type: "Dinosaur", picture: "" }
-           ];
+             { "id": 2957, "name": "widget", "price": 32, "quantity": 203, "color": "red", "discount": 31 },
+             { "id": 89274, "name": "golf club", "price": 98, "quantity": 10, "color": "black", "discount": 0 },
+             { "id": 64, "name": "iPhone", "price": 499, "quantity": 2, "color": "white", "discount": 0 },
+             { "id": 87363, "name": "bonsai tree", "price": 76, "quantity": 2, "color": "green", "discount": 0 },
+             { "id": 1736, "name": "towel", "price": 55, "quantity": 30, "color": "brown", "discount": 10 },
+             { "id": 4836, "name": "dog bed", "price": 99, "quantity": 10, "color": "beige", "discount": 50 },
+             { "id": 829, "name": "waste basket", "price": 15, "quantity": 40, "color": "silver", "discount": 0 },
+             { "id": 46, "name": "guitar", "price": 899, "quantity": 3, "color": "blue", "discount": 150 },
+             { "id": 17456, "name": "matcha tea", "price": 42, "quantity": 4, "color": "green", "discount": 11 },
+             { "id": 3292, "name": "enlightenment", "price": 99999, "quantity": 1, "color": "red", "discount": 0 },
+             { "id": 533, "name": "eggs", "price": 5, "quantity": 12, "color": "brown", "discount": 1 },
+             { "id": 683, "name": "pillow", "price": 27, "quantity": 10, "color": "black", "discount": 12 }
+          ];
 
-           this.previewLimit = 5;
+          this.newItem = {
+            name: "",
+            price: "",
+            quantity: "",
+            color: "",
+            discount: ""
+          };
 
-          this.types = ['Mammal', 'Bird', '???'];
-        // var animal = animals[0];
-        //
-        // this.name = animal.species;
-        // this.type = animal.type;
-        // this.picture = animal.picture;
+          this.tax = 0.0575;
+
+          this.priceConvert = function priceConvert(item) {
+              return (item.price - item.discount) + ( (item.price - item.discount) * this.tax );
+          };
+
+          this.GBP = false;
+          this.color = 'Color';
+
+          this.convert = function convert() {
+            if(this.GBP === false) {
+              this.GBP = true;
+              this.color = 'Colour';
+            } else {
+              this.GBP = false;
+              this.color = 'Color';
+            }
+          };
+
+          this.addNewItem = function addNewItem(form) {
+            console.log("Saving");
+            if (form.$valid) {
+            this.all.push(this.newItem);
+            this.newItem = {};
+            form.$setPristine();
+            form.$setUntouched();
+          } else {
+            console.log("Something was wrong! Fix your form!");
+          }
+          };
+
+          this.plusOne = function plusOne(merch) {
+            merch.quantity++;
+          };
+
+          this.minusOne = function plusOne(merch) {
+            if (merch.quantity === 0) {
+              merch.quantity = 0;
+            } else {
+            merch.quantity--;
+          }
+          };
+
       }
 })();
